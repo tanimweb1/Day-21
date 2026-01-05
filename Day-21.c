ca763eba69368ac560d20030afdf80491fb02ae2
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2026-01-05 22:41:06 +0600

    Update Day-21.c

--- Day-21.c
+++ Day-21.c
@@ -2,9 +2,30 @@
  int main(){
 
 
+    // max & min in array
 
+    int numbers[] = {23, 45, 65, 75, 43, 56, 354, 645, 56, 43};
+
+    int max = numbers[0];
+    int min = numbers[0];
+
+    for (int i = 1; i < 10; i++)
+    {
+        if (max < numbers[i])
+        {
+            max = numbers[i];
+        }
+        if (min > numbers)
+        {
+            min = numbers[i];
+        }
+    }
+
+    printf("Max is %d\n", max);
+    printf("Min is %d\n", min);
 
 
 
     return 0;
+
  }
